3540fcb7ef4f5a82bdb3e3c4221cdd1399f37179
hisato_kozuki <q66yuan@gmail.com>
2025-05-02 17:01:01 +0900

    予定を一週間ごとに区切り土日に印をつけるようにしました。

--- main.js
+++ main.js
@@ -34,7 +34,10 @@ function get_events(){
     let received_data;
     fetch(url, options)
     .then(response => response.text())
-    .then(data => {dbsave(received_data=JSON.parse(data));console.log(received_data);display(received_data);})
+    .then(data => {
+        dbsave(received_data=JSON.parse(data));console.log(received_data);display(received_data);
+        document.getElementById("postbutton").innerText = "送信";
+    })
     .catch(error => console.error("Error:", error));
 }
 
@@ -46,8 +49,8 @@ function post_event(data){
     fetch(url, options)
     .then(response => response.text())
     .then(data => {
-        console.log(data);console.log();alert(data);get_events();
-        document.getElementById("postbutton").innerText = "送信";
+        console.log(data);get_events();
+        document.getElementById("postbutton").innerText = "完了";
     })
     .catch(error => console.error("Error:", error));
 }
@@ -60,8 +63,8 @@ function delete_event(data){
     fetch(url, options)
     .then(response => response.text())
     .then(data => {
-        console.log(data);console.log();alert(data);get_events();
-        delete_cell.innerText = "削除";
+        console.log(data);get_events();
+        delete_cell.innerText = "完了";
     })
     .catch(error => console.error("Error:", error));
 }
@@ -75,11 +78,27 @@ function display(events){
     for(let i = 0; i < events.length; i++){
         let date_cell = document.createElement("div");
         let event_cell = document.createElement("div");
+        let dot = document.createElement("p");
         let div = document.createElement("div");
         date_cell.className = "date_cell";
         event_cell.className = "event_cell";
         div.style.display = "flex";
-        date_cell.innerText = events[i].year + "/" + (events[i].month+1).toString().padStart(2, "0") + "/" + events[i].date.toString().padStart(2, "0") + "  " + events[i].hour.toString().padStart(2, "0") + "\n";
+        dot.innerText = "◆"; dot.style.margin = "0px"; dot.style.fontSize = "20px";
+        dot.style.width = "3%"; dot.style.alignSelf = "center"; dot.style.textAlign = "center";
+        if ((events[i].day+6)%7 == 6)dot.style.color = "orangered";
+        else if ((events[i].day+6)%7 == 5)dot.style.color = "darkturquoise";
+        else dot.innerText = "";
+        div.appendChild(dot);
+        date_cell.innerText += events[i].year[0] + "/" + (events[i].month[0]+1).toString().padStart(2, "0") + "/" + events[i].date[0].toString().padStart(2, "0") + " - " + events[i].hour[0].toString().padStart(2, "0");
+        if(events[i].year[0] != events[i].year[1]){
+            date_cell.innerText += "\n~ " + events[i].year[1] + "/" + (events[i].month[1]+1).toString().padStart(2, "0") + "/" + events[i].date[1].toString().padStart(2, "0") + " - " + events[i].hour[1].toString().padStart(2, "0") + "\n";
+        }else if(events[i].month[0] != events[i].month[1]){
+            date_cell.innerText += "\n~ " + (events[i].month[1]+1).toString().padStart(2, "0") + "/" + events[i].date[1].toString().padStart(2, "0") + " - " + events[i].hour[1].toString().padStart(2, "0") + "\n";
+        }else if(events[i].date[0] != events[i].date[1]){
+            date_cell.innerText += "\n~ " + events[i].date[1].toString().padStart(2, "0") + " - " + events[i].hour[1].toString().padStart(2, "0") + "\n";
+        }else if(events[i].hour[0] != events[i].hour[1]){
+            date_cell.innerText += " ~ " + events[i].hour[1].toString().padStart(2, "0") + "\n";
+        }
         let color = colorcode[events[i].color];
         if(color == undefined)color = "#404040";
         event_cell.innerHTML = "<span style='color: " + color + "'>" + events[i].title +"</span><br>";
@@ -95,11 +114,26 @@ function display(events){
                     'type': "delete",
                     'id': events[i].id
                 };
-                delete_cell.innerText = "お待ちください。";
+                delete_cell.innerText = "……";
                 delete_event(data);
             }
         });
         div.appendChild(delete_cell);
+        let date = [new Date(), new Date()];
+        date[0].setFullYear(events[i].year[0]);
+        date[0].setMonth(events[i].month[0]);
+        date[0].setDate(events[i].date[0]);
+        if(i){
+            date[1].setFullYear(events[i-1].year[0]); 
+            date[1].setMonth(events[i-1].month[0]);
+            date[1].setDate(events[i-1].date[0]);
+            console.log((date[0] - date[1])/86400000)
+            if((date[0] - date[1])/86400000 - (events[i].day+6)%7 > 0){
+                let div = document.createElement("div");
+                div.style.borderBottom = "solid 1px gray";
+                cell.appendChild(div);
+            }
+        }
         cell.appendChild(div);
     }
 
@@ -121,7 +155,7 @@ document.getElementById("form").addEventListener('submit', (event) => {
         'color': document.getElementById("form").color.value,
     };
     if(data.title != ""){
-        document.getElementById("postbutton").innerText = "お待ちください。";
+        document.getElementById("postbutton").innerText = "……";
         post_event(data);
     }
 });
